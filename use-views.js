ee72babcddf948d76c3375e2f99d54a720247dfe
Elder Rivero <branimir.rivero@missionary.org>
2025-06-24 17:14:05 -0400

    Areas view info

--- use-views.js
+++ use-views.js
@@ -1,6 +1,7 @@
 const views = [
   { name: "Zones", file: "views-explanation/zones.html" },
   { name: "Districts", file: "views-explanation/Districts.html" },
+  { name: "Areas", file: "views-explanation/Areas.html" },
   { name: "Tasks", file: "views-explanation/Tasks.html" },
   { name: "Calendar", file: "views-explanation/Calendar.html" },
   { name: "Contacts", file: "views-explanation/Contacts.html" }
